9f74942a903cfe05799f00180287f117070ae052
Erik <erikwjonsson@gmail.com>
2016-12-09 17:57:09 +0100

    Changed intendation.

--- integer_to_english.rb
+++ integer_to_english.rb
@@ -1,14 +1,14 @@
 
 class EnglishNumber
 
-   def initialize(integer)
-   		if integer < 100.0 or integer > -100.0
-        integer = turn_negative_to_positive(integer)
-        @integer = round_of_if_necessary(integer)
-   		else
-        @integer = nil
-      end
-   end
+  def initialize(integer)
+  	if integer < 100.0 or integer > -100.0
+      integer = turn_negative_to_positive(integer)
+      @integer = round_of_if_necessary(integer)
+  	else
+      @integer = nil
+    end
+  end
 
 	def in_english
 		unless @integer.nil?
@@ -16,42 +16,42 @@ class EnglishNumber
 		end
 
     integers_to_english = { # Could have probably done this with YAML.
-        90 => "ninety",
-        80 => "eighty",
-        70 => "seventy",
-        60 => "sixty",
-        50 => "fifty",
-        40 => "forty",
-        30 => "thirty",
-        20 => "twenty",
-        19 => "nineteen",
-        18 => "eighteen",
-        17 => "seventeen", 
-        16 => "sixteen",
-        15 => "fifteen",
-        14 => "fourteen",
-        13 => "thirteen",              
-        12 => "twelve",
-        11 => "eleven",
-        10 => "ten",
-        9 => "nine",
-        8 => "eight",
-        7 => "seven",
-        6 => "six",
-        5 => "five",
-        4 => "four",
-        3 => "three",
-        2 => "two",
-        1 => "one",
-      }
+      90 => "ninety",
+      80 => "eighty",
+      70 => "seventy",
+      60 => "sixty",
+      50 => "fifty",
+      40 => "forty",
+      30 => "thirty",
+      20 => "twenty",
+      19 => "nineteen",
+      18 => "eighteen",
+      17 => "seventeen", 
+      16 => "sixteen",
+      15 => "fifteen",
+      14 => "fourteen",
+      13 => "thirteen",              
+      12 => "twelve",
+      11 => "eleven",
+      10 => "ten",
+      9 => "nine",
+      8 => "eight",
+      7 => "seven",
+      6 => "six",
+      5 => "five",
+      4 => "four",
+      3 => "three",
+      2 => "two",
+      1 => "one",
+    }
 
-	    if @integer.nil?
-	    	@integer # Example => nil
-	    elsif integers_to_english[remain].nil?
-	    	return integers_to_english[whole] # Example 1 => one, 19 => nineteen
-	    else 
-	    	return integers_to_english[whole] + integers_to_english[remain] # Example twenty + two => twentytwo
-	    end
+    if @integer.nil?
+    	@integer # Example => nil
+    elsif integers_to_english[remain].nil?
+    	return integers_to_english[whole] # Example 1 => one, 19 => nineteen
+    else 
+    	return integers_to_english[whole] + integers_to_english[remain] # Example twenty + two => twentytwo
+    end
 	end
 
 	private
